b34947ef5b0d71f24a290f243b7794001533c21d
stonarini <stonarini@cifpfbmoll.eu>
2022-02-28 20:37:10 +0100

    chore: create files required by tox

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="freshful-api",
+    version="1.0",
+    description="Basic RESTful API with Flask",
+    author="stonarini",
+    author_email="stonarini@cifpfbmoll.eu",
+    url="htts://github.com/stonarini/freshful-api",
+)
