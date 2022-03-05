e091fda2286a6071672229ab1b3b3def30baef9a
stonarini <stonarini@cifpfbmoll.eu>
2022-03-05 20:04:14 +0100

    feat(services): add intermediate class Services for db querying

---
+++ services/services.py
@@ -0,0 +1,14 @@
+from flask import g
+from repository import get_db
+
+
+class Services:
+    @staticmethod
+    def get_items():
+        get_db()
+        return list(item for item in g.db.find({}, {"_id": False}))
+
+    @staticmethod
+    def get_item(title):
+        get_db()
+        return list(g.db.find({"title": title}, {"_id": False}))
