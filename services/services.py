4994e29e741a4779166938324db08e1da9fbbd5f
stonarini <stonarini@cifpfbmoll.eu>
2022-03-12 19:33:49 +0100

    feat(services): create CRUD calls using Introduce Parameter Object

--- services/services.py
+++ services/services.py
@@ -1,14 +1,19 @@
-from flask import g
-from repository import get_db
+from repository import DB
 
 
 class Services:
     @staticmethod
     def get_items():
-        get_db()
-        return list(g.db.find({}, {"_id": False}))
+        return list(DB.get_all())
 
     @staticmethod
-    def get_item(title):
-        get_db()
-        return list(g.db.find({"title": title}, {"_id": False}))
+    def get_item(item):
+        return list(DB.get_one(item))
+
+    @staticmethod
+    def update_item(item):
+        return list(DB.update_one(item))
+
+    @staticmethod
+    def delete_item(item):
+        return list(DB.delete_one(item))
