b0fd1d1090af1e521484b09dfe83c87b4ed4785a
stonarini <stonarini@cifpfbmoll.eu>
2022-03-12 23:40:33 +0100

    feat(db): add methods to find and create item

--- repository/db.py
+++ repository/db.py
@@ -4,11 +4,39 @@ from .connection import get_db
 class DB:
     @staticmethod
     def get_all():
-        pass
+        db = get_db()
+        return db.find({}, {"_id": False})
 
     @staticmethod
-    def get_one():
-        pass
+    def get_one(item):
+        db = get_db()
+        return db.find_one(
+            {
+                "name": item.get_name(),
+                "sell_in": item.get_sell_in(),
+                "quality": item.get_quality(),
+            },
+            {"_id": False},
+        )
+
+    @staticmethod
+    def get_by_name(name):
+        db = get_db()
+        return db.find(
+            {"name": name},
+            {"_id": False},
+        )
+
+    @staticmethod
+    def create_one(item):
+        db = get_db()
+        db.insert_one(
+            {
+                "name": item.get_name(),
+                "sell_in": item.get_sell_in(),
+                "quality": item.get_quality(),
+            }
+        )
 
     @staticmethod
     def update_one():
