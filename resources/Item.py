e1e0c5cd13958b9f9d5d02dfdf3ceca3bb71bff1
stonarini <stonarini@cifpfbmoll.eu>
2022-03-12 23:38:07 +0100

    feat(Item resource): add put method handling

--- resources/Item.py
+++ resources/Item.py
@@ -5,3 +5,6 @@ from services import Services
 class Item(ItemResource):
     def get(self, item):
         return Services.get_item(item)
+
+    def put(self, item):
+        return Services.create_item(item)
