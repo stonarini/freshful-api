4c3ddcdb848d477c522f88ad2c6608a39b0dab7f
stonarini <stonarini@cifpfbmoll.eu>
2022-03-14 20:09:55 +0100

    refactor(db): simplify item creation by using built in __dict__ function of python

--- repository/db.py
+++ repository/db.py
@@ -10,33 +10,18 @@ class DB:
     @staticmethod
     def get_one(item):
         db = get_db()
-        return db.find_one(
-            {
-                "name": item.get_name(),
-                "sell_in": item.get_sell_in(),
-                "quality": item.get_quality(),
-            },
-            {"_id": False},
-        )
+        return db.find_one(item.__dict__, {"_id": False})
 
     @staticmethod
-    def get_by_name(name):
+    def filter_item(item):
         db = get_db()
-        return db.find(
-            {"name": name},
-            {"_id": False},
-        )
+        item_dict = dict(filter(lambda a: a[1], item.__dict__.items()))
+        return db.find(item_dict, {"_id": False})
 
     @staticmethod
     def create_one(item):
         db = get_db()
-        db.insert_one(
-            {
-                "name": item.get_name(),
-                "sell_in": item.get_sell_in(),
-                "quality": item.get_quality(),
-            }
-        )
+        db.insert_one(item.__dict__)
 
     @staticmethod
     def update_one():
