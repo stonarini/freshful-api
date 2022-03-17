8c96689b179e8770981075f31e77f83542f03eb1
stonarini <stonarini@cifpfbmoll.eu>
2022-03-18 00:00:53 +0100

    fix(schema): update deprecated 'default' parameter to 'dump_default'

--- resources/schema.py
+++ resources/schema.py
@@ -4,8 +4,8 @@ from domain import AgedBrie, Backstage, ConjuredItem, Items, Sulfuras, NormalIte
 
 class ItemSchema(Schema):
     name = fields.Str(required=True)
-    sell_in = fields.Int(required=False, default=None, allow_none=True)
-    quality = fields.Int(required=False, default=None, allow_none=True)
+    sell_in = fields.Int(required=False, dump_default=None, allow_none=True)
+    quality = fields.Int(required=False, dump_default=None, allow_none=True)
 
     @post_load
     def make_item(self, item_args, **kwargs):
