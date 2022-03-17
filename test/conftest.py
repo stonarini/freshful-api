d0b17f1d0674f79c57e86db8c20c440e11231f39
stonarini <stonarini@cifpfbmoll.eu>
2022-03-18 00:03:21 +0100

    feat(conftest): add session-scoped fixtures to get flask test client and load the db connection

---
+++ test/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from app import create_app
+from repository.db import init_db, drop_db
+
+
+@pytest.fixture(scope="session")
+def app():
+    app = create_app()
+    app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
+
+    yield app
+
+
+@pytest.fixture(scope="session")
+def client(app):
+    with app.app_context():
+        init_db("TestGildedRose")
+
+        yield app.test_client()
+
+        drop_db("TestGildedRose")
